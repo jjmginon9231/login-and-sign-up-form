bd7aa56788516dfb7abfa54230bcb3713c3d0886
jjmginon <jjmginon9231@gmail.com>
2024-07-10 17:08:38 +0800

    feat: add regex and minify code
    Add regex for name validation. Add addEventListener for password
    strength. Add variables and minify code to improve performance.

--- js/script.js
+++ js/script.js
@@ -1,121 +1,219 @@
 // SHOW/HIDE PASSWORD (WITH ICON CHANGE)
+
 const container = document.querySelector(".container"),
     passShowHide = document.querySelectorAll(".showHidePass"),
     passFields = document.querySelectorAll(".password"),
     signUp = document.querySelector(".sign-up-tab"),
     login = document.querySelector(".login-tab");
 
-passShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        passFields.forEach(passField => {
-            if (passField.type === "password") {
-                passField.type = "text";
-                passShowHide.forEach(icon => {
-                    icon.classList.replace("uil-eye-slash", "uil-eye");
-                });
-            } else {
-                passField.type = "password";
-                passShowHide.forEach(icon => {
-                    icon.classList.replace("uil-eye", "uil-eye-slash");
-                });
-            }
-        });
-    });
-});
+passShowHide.forEach((eyeIcon => {
+    eyeIcon.addEventListener("click", (() => {
+        passFields.forEach((eyeIcon => {
+            "password" === eyeIcon.type ? (eyeIcon.type = "text", passShowHide.forEach((eyeIcon => {
+                eyeIcon.classList.replace("uil-eye-slash", "uil-eye")
+            }))) : (eyeIcon.type = "password", passShowHide.forEach((eyeIcon => {
+                eyeIcon.classList.replace("uil-eye", "uil-eye-slash")
+            })))
+        }))
+    }))
+}));
 
 // LOGIN EMAIL VERIFICATION
-const email = document.getElementById('email-login');
+
+const email = document.getElementById("email-login"),
+    emailAlert1 = document.getElementById("email_alert1"),
+    emailAlert2 = document.getElementById("email_alert2"),
+    loginButton = document.getElementById("login-button");
 
 function validate_email() {
-    let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g;
-    if (!email.value.match(pattern)) {
-        document.getElementById('email_alert1').innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 10px; color: #dc143c;"></i>';
-        document.getElementById('email_alert2').innerHTML = 'Your email address is invalid';
-        document.getElementById('login-button').disabled = true;
-        document.getElementById('login-button').style.opacity = (0.4);
-    } else {
-        document.getElementById('email_alert1').innerHTML = '<i class="uil uil-check-circle" style="right: 10px; color: #00b300;"></i>';
-        document.getElementById('email_alert2').innerHTML = 'Your email address is valid';
-        document.getElementById('login-button').disabled = false;
-        document.getElementById('login-button').style.opacity = (1);
-    }
+    email.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g)
+        ?
+        (
+            emailAlert1.innerHTML = '<i class="uil uil-check-circle" style="right: 10px; color: #00b300;"></i>',
+            document.getElementById("email_alert2").innerHTML = "You entered a valid email address.",
+            loginButton.disabled = !1, loginButton.style.opacity = 1
+        )
+        :
+        (
+            emailAlert1.innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 10px; color: #dc143c;"></i>',
+            emailAlert2.innerHTML = "You entered an invalid email address!",
+            loginButton.disabled = !0,
+            loginButton.style.opacity = 0.4
+        )
 }
 
 // REMEMBER ME CHECKBOX FUNCTION
+
 const rmCheck = document.getElementById("logCheck"),
     emailInput = document.getElementById("email-login");
 
-if (localStorage.checkbox && localStorage.checkbox !== "") {
-    rmCheck.setAttribute("checked", "checked");
-    emailInput.value = localStorage.username;
-} else {
-    rmCheck.removeAttribute("checked");
-    emailInput.value = "";
+function lsRememberMe() {
+    rmCheck.checked && "" !== emailInput.value
+        ?
+        (localStorage.username = emailInput.value, localStorage.checkbox = rmCheck.value)
+        :
+        (localStorage.username = "", localStorage.checkbox = "")
 }
 
-function lsRememberMe() {
-    if (rmCheck.checked && emailInput.value !== "") {
-        localStorage.username = emailInput.value;
-        localStorage.checkbox = rmCheck.value;
-    } else {
-        localStorage.username = "";
-        localStorage.checkbox = "";
-    }
-}
+localStorage.checkbox && "" !== localStorage.checkbox
+    ?
+    (rmCheck.setAttribute("checked", "checked"), emailInput.value = localStorage.username)
+    :
+    (rmCheck.removeAttribute("checked"), emailInput.value = "");
 
 // LOGIN AND SIGN UP CHANGE TAB
-signUp.addEventListener("click", () => {
-    container.classList.add("active");
-});
-login.addEventListener("click", () => {
-    container.classList.remove("active");
-});
 
-// SIGN UP EMAIL VERIFICATION
-const emailReg = document.getElementById('email-sign-up');
+signUp.addEventListener("click", (() => {
+    container.classList.add("active")
+})),
+    login.addEventListener("click", (() => {
+        container.classList.remove("active")
+    }));
 
-function validate_emailReg() {
-    let pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g;
-    if (!emailReg.value.match(pattern)) {
-        document.getElementById('email_alert3').innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 10px; color: #dc143c;"></i>';
-        document.getElementById('email_alert4').innerHTML = 'Your email address is invalid';
-        document.getElementById('sign-up-button').disabled = true;
-        document.getElementById('sign-up-button').style.opacity = (0.4);
-    } else {
-        document.getElementById('email_alert3').innerHTML = '<i class="uil uil-check-circle" style="right: 10px; color: #00b300;"></i>';
-        document.getElementById('email_alert4').innerHTML = 'Your email address is valid';
-        document.getElementById('sign-up-button').disabled = false;
-        document.getElementById('sign-up-button').style.opacity = (1);
-    }
+// NAME VALIDATION
+
+const nameReg = document.getElementById('name'),
+    signUpButton = document.getElementById('sign-up-button'),
+    nameAlert3 = document.getElementById('name_alert3'),
+    nameAlert4 = document.getElementById('name_alert4');
+
+function validate_nameReg() {
+    // Full Name
+    let pattern1a = /^(?!([A-Z][a-z]*|[A-Z]\.)$)(?!([A-Z][a-z]* [A-Z]\.)$)[A-Z][a-z]+(?: [A-Z]\.)?(?: [A-Z][a-z]+)*$/;
+    // Full Name with Middle Initial
+    let pattern1b = /^(?!([A-Z][a-z]*|[A-Z]\.)$)(?!([A-Z][a-z]* [A-Z]\.)$)[A-Z][a-z]+(?: [A-Z][a-z]+| [A-Z]'[A-Za-z]{2,})*$/;
+    //Full Name with Hyphenated Middle Name
+    let pattern1c = /^(?!([A-Z][a-z]*|[A-Z]\.)$)(?!([A-Z][a-z]* [A-Z]\.)$)[A-Z][a-z]+(?: [A-Z]\.)?(?: [A-Z][a-z]+)*(?:-[A-Z][a-z]+)?$/;
+    // Chinese
+    let pattern2 = /(\p{Script=Hani})+/gu;
+    // Korean
+    let pattern3 = /[\u3131-\uD79D]/ugi;
+    // Japanese
+    let pattern4 = /[一-龠]+|[ぁ-ゔ]+|[ァ-ヴー]+|[々〆〤ヶ]+/u;
+
+    nameReg.value.match(pattern1a)
+        || nameReg.value.match(pattern1b)
+        || nameReg.value.match(pattern1c)
+        || nameReg.value.match(pattern2)
+        || nameReg.value.match(pattern3)
+        || nameReg.value.match(pattern4)
+        ?
+        (
+            nameReg.style.border = "2px solid #00b300",
+            nameAlert3.innerHTML = '<i class="uil uil-check-circle" style="right: 10px; color: #00b300;"></i>',
+            nameAlert4.innerHTML = "You entered a valid name.",
+            signUpButton.disabled = !1,
+            signUpButton.style.opacity = 1
+        )
+        :
+        (
+            nameReg.style.border = "1px solid #dc143c",
+            nameAlert3.innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 10px; color: #dc143c;"></i>',
+            nameAlert4.innerHTML = "You entered an invalid name!",
+            signUpButton.disabled = !0,
+            signUpButton.style.opacity = 0.4
+        );
 }
 
+// SIGN UP EMAIL VERIFICATION
+
+const emailReg = document.getElementById('email-sign-up'),
+    emailAlert3 = document.getElementById('email_alert3'),
+    emailAlert4 = document.getElementById('email_alert4');
+
+function validate_emailReg() {
+    emailReg.value.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/g)
+        ?
+        (
+            emailReg.style.border = "2px solid #00b300",
+            emailAlert3.innerHTML = '<i class="uil uil-check-circle" style="right: 10px; color: #00b300;"></i>',
+            emailAlert4.innerHTML = "Your email address is valid.",
+            signUpButton.disabled = !1,
+            signUpButton.style.opacity = 1
+        )
+        :
+        (
+            emailReg.style.border = "1px solid #dc143c",
+            emailAlert3.innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 10px; color: #dc143c;"></i>',
+            emailAlert4.innerHTML = "Your email address is invalid!",
+            signUpButton.disabled = !0,
+            signUpButton.style.opacity = 0.4
+        )
+}
+
+// PASSWORD STRENGTH
+
+const passSign = document.getElementById("password-sign-up"),
+    message = document.getElementById("message"),
+    strength = document.getElementById("strength");
+
+passSign.addEventListener("input", (() => {
+    passSign.value.length > 0 ? message.style.display = "block"
+        :
+        message.style.display = "none",
+        passSign.value.length < 6
+            ?
+            (
+                strength.innerHTML = "weak!",
+                passSign.style.border = "1px solid #dc143c", message.style.color = "#dc143c"
+            )
+            :
+            passSign.value.length >= 6 && passSign.value.length < 8
+                ?
+                (
+                    strength.innerHTML = "good.",
+                    passSign.style.border = "1.75px solid #daa520",
+                    message.style.color = "#daa520"
+                )
+                :
+                passSign.value.length > 8 && (
+                    strength.innerHTML = "strong!",
+                    passSign.style.border = "2px solid #00b300",
+                    message.style.color = "#00b300"
+                )
+}));
+
 // PASSWORD VERIFICATION
+
+const passCon = document.getElementById('password-confirm'),
+    passAlert1 = document.getElementById('pass_alert1'),
+    passAlert2 = document.getElementById('pass_alert2');
+
 function validate_password() {
 
     let password = document.getElementById('password-sign-up').value;
     let confirmPassword = document.getElementById('password-confirm').value;
-    if (password != confirmPassword) {
-        document.getElementById('password-confirm').style.border = '2px solid #DC143C';
-        document.getElementById('pass_alert1').innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 35px; color: #dc143c;"></i>';
-        document.getElementById('pass_alert2').innerHTML = 'password does not match';
-        document.getElementById('sign-up-button').disabled = true;
-        document.getElementById('sign-up-button').style.opacity = (0.4);
-    } else {
-        document.getElementById('password-confirm').style.border = '2px solid #00b300';
-        document.getElementById('pass_alert1').innerHTML = '<i class="uil uil-check-circle" style="right: 35px; color: #00b300;"></i>';
-        document.getElementById('pass_alert2').innerHTML = 'password matched';
-        document.getElementById('sign-up-button').disabled = false;
-        document.getElementById('sign-up-button').style.opacity = (1);
-    }
+
+    password != confirmPassword
+        ?
+        (
+            passCon.style.border = "2px solid #DC143C",
+            passAlert1.innerHTML = '<i class="uil uil-exclamation-triangle" style="right: 35px; color: #dc143c;"></i>',
+            passAlert2.innerHTML = "Password does not match",
+            signUpButton.disabled = !0,
+            signUpButton.style.opacity = 0.4
+        )
+        :
+        (
+            passCon.style.border = "2px solid #00b300",
+            passAlert1.innerHTML = '<i class="uil uil-check-circle" style="right: 35px; color: #00b300;"></i>',
+            passAlert2.innerHTML = "Password matched",
+            signUpButton.disabled = !1,
+            signUpButton.style.opacity = 1
+        );
 }
 
 // TERMS AND CONDITIONS CHECKBOX FUNCTION
+
+const tacAlert1 = document.getElementById('tac_alert1'),
+    tacAlert2 = document.getElementById('tac_alert2');
+
 function checkForm(form) {
-    if (!form.agree.checked) {
-        document.getElementById('tac_alert1').innerHTML = '<p  style="font-size: 0.625rem; font-style: italic; right: 35px; color: #dc143c;">Required*</p>';
-        document.getElementById('tac_alert2').innerHTML = 'Please indicate that you accept the Terms and Conditions';
-        return false;
-    } else {
-        return true;
-    }
+    return !!form.agree.checked
+        ||
+        (
+            tacAlert1.innerHTML = '<p style="font-size: 0.625rem; font-style: italic; right: 35px; color: #dc143c;">Required*</p>',
+            tacAlert2.innerHTML = "Please indicate that you accept the Terms and Conditions",
+            !1
+        )
 }
